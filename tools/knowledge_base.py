312d115eefa683574b23406b2ad585c7c4a53212
mysteryisfun <ujwaljeevan123@gmail.com>
2025-07-17 22:01:22 +0530

    FAISS to chroma

---
+++ tools/knowledge_base.py
@@ -0,0 +1,52 @@
+from langchain_core.tools import tool
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import os
+_retriever = None
+from dotenv import load_dotenv
+load_dotenv()
+
+def _initialize_retriever():
+    """Loads the document, splits it, creates embeddings, and builds retriever"""
+    
+    global _retriever
+    if _retriever is not None:
+        return _retriever
+    print("Initializing Knowledge Base Retriever...")
+    
+    loader = TextLoader("docs/nuclear_energy.txt")
+    documents = loader.load()
+    
+    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+    docs = text_splitter.split_documents(documents)
+    
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=os.getenv("google_api_key"))
+    
+    db = Chroma.from_documents(docs, embeddings, persist_directory="chroma_db")
+    _retriever = db.as_retriever(search_kwargs={"k": 1})
+    return _retriever
+
+@tool
+def knowledge_base_search(query: str) -> str:
+    """
+    Searches the knowledge base document (a report on AI ethics in urban planning)
+    to find specific information, definitions, or arguments related to the report.
+    Use this tool when you need to cite specific details from the official report.
+
+    Args:
+        query: The specific question or topic to look up in the knowledge base.
+    """
+    print(f"\033[32m--- Executing Knowledge Base Search with query: '{query}' ---\033[0m")
+    retriever = _initialize_retriever()
+    
+    docs = retriever.invoke(query)
+    return "\n\n".join([doc.page_content for doc in docs])
+
+if __name__ == "__main__":
+    print("testing KB")
+    _initialize_retriever()
+    query = "What are the main ethical concerns regarding nuclear energy"
+    
+    print(knowledge_base_search(query))
